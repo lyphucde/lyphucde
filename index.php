4b938476073d625a90d7a48c74080670b986b9f3
lyphucde <hotro.vn.tk@gmail.com>
2025-08-15 08:33:50 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('head.php');?>
+<title><?=$site_tenweb;?></title>
+
+<div class="container">
+<img width="100%" src="/img/1.png"/>
+
+<div class="home-title">
+    <span class="color3">Thông báo</span>
+</div>
+
+<div class="alert-home">
+<?=$site_thongbao;?>
+</div>
+
+<?php require_once('./system/doi-the-auto.php');?>
+
+<?php include('foot.php');?>
